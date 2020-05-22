57d23d7d72cf5b315ed81498c5b1ed4d7366e66c
sxd422638 <1623130592@qq.com>
2020-05-22 21:06:08 +0800

    string

--- 03string类/03string类/test.cpp
+++ 03string类/03string类/test.cpp
@@ -1,7 +1,7 @@
-//#include<iostream>
-//#include<string>
-//#include<assert.h>
-//using namespace std;
+#include<iostream>
+#include<string>
+#include<assert.h>
+using namespace std;
 
 
 //int main()
@@ -37,22 +37,22 @@
 //	//cout << s2 << endl;
 //
 //
-//	//string s;
-//	////s.reserve(1000);//使用前提是我知道需要多少空间，这是可以省去增容的开销
-//	//size_t sz = s.capacity();
-//	//cout << "making s grow:\n";
-//	////string 的其中一个缺点，插入数据，空间不够需要增容，增容是有性能消耗的
-//	////s.resize(1000); ->s[i] = 'c';
-//	//for (int i = 0; i < 100; ++i)
-//	//{
-//	//	s.push_back('c');
-//	//	//s[i] = 'c';
-//	//	if (sz != s.capacity())
-//	//	{
-//	//		sz = s.capacity();
-//	//		cout << "capacity changed: " << sz << '\n';
-//	//	}
-//	//}
+	//string s;
+	////s.reserve(1000);//使用前提是我知道需要多少空间，这是可以省去增容的开销
+	//size_t sz = s.capacity();
+	//cout << "making s grow:\n";
+	////string 的其中一个缺点，插入数据，空间不够需要增容，增容是有性能消耗的
+	////s.resize(1000); ->s[i] = 'c';
+	//for (int i = 0; i < 100; ++i)
+	//{
+	//	s.push_back('c');
+	//	//s[i] = 'c';
+	//	if (sz != s.capacity())
+	//	{
+	//		sz = s.capacity();
+	//		cout << "capacity changed: " << sz << '\n';
+	//	}
+	//}
 ////3.string类的遍历操作
 //	//转换成整形
 //	//string s("12345");
@@ -76,8 +76,8 @@
 //	//cout << s1 << endl;
 //	string file1("string.c.doc.txt");
 //	string file2("string.c.doc.txt");
-//	size_t pos1 = file1.find('.');
-//	size_t pos2 = file1.rfind('.');
+//	size_t pos1 = file1.find('.');从字符串pos位置开始往后找字符c，返回该字符在字符串中的位置
+//	size_t pos2 = file1.rfind('.');从字符串pos位置开始往前找字符c，返回该字符在字符串中的位置
 //	cout << file1.substr(pos1) << endl;
 //	cout << file1.substr(pos2) << endl;
 //
@@ -104,17 +104,17 @@
 //	cout << url3.substr(urlpos3, urlend3 - urlpos3) << endl;
 //
 //
-//	//6.string的使用
-//	//	1.构造/拷贝
-//	//	2.增->operator+= /insert
-//	//	3.删->earse
-//	//	4.查->find/rfind
-//	//	5.改->operator[] 
-//	//	6.遍历->size() + operator[]  了解迭代器遍历  范围for
-//	//	7.输入输出 cin>>  getline(当输入字符串包含空格时用它)  cout<<
-//	//	8.容量，增容规则 -> VS基本1.5倍增  g++基本2倍增  reserve+resize
-//	//	9.c_str()会返回对象中指向字符数组的指针
-//	//  10.重载了 > < == 的比较 按ascll码去比较
+	/*6.string的使用
+		1.构造/拷贝
+		2.增->operator+= /insert
+		3.删->earse
+		4.查->find/rfind
+		5.改->operator[] 
+		6.遍历->size() + operator[]  了解迭代器遍历  范围for
+		7.输入输出 cin>>  getline(当输入字符串包含空格时用它)  cout<<
+		8.容量，增容规则 -> VS基本1.5倍增  g++基本2倍增  reserve+resize
+		9.c_str()会返回对象中指向字符数组的指针
+	  10.重载了 > < == 的比较 按ascll码去比较*/
 //
 //
 //
@@ -233,7 +233,7 @@
 //		{}*/
 //
 //		//string(char* str = nullptr) // 这里不能给nullptr，strlen(str)就崩溃了
-//		string(char* str = "")
+//		string(char* str = "") // 这里给一个缺省值
 //			:_str(new char[strlen(str)+1])
 //		{
 //			strcpy(_str, str);
@@ -269,10 +269,10 @@
 //			_str = nullptr;
 //		}
 //
-//		char& operator[](size_t pos)
-//		{
-//			return _str[pos];
-//		}
+		/*char& operator[](size_t pos)
+		{
+			return _str[pos];
+		}*/
 //
 //		const char& operator[](size_t pos) const
 //		{
@@ -301,14 +301,14 @@
 //
 //int main()
 //{
-//	bit::string s1("world");
-//	bit::string s2("hello");
+//	hdzc::string s1("world");
+//	hdzc::string s2("hello");
 //	s2[0] = 'x';
 //
 //	cout << sizeof(s1) << endl;
 //	cout << sizeof(s2) << endl;
 //
-//	bit::string copy(s2);
+//	hdzc::string copy(s2);
 //	cout << s1 << endl;
 //	cout << s2 << endl;
 //	cout << copy << endl;
@@ -347,15 +347,15 @@ namespace hdzc
 			return _str + _size;
 		}
 
-		string(const char* str = "")
-			:_size(strlen(str))
+		string(const char* str = "")// 构造函数 实际当中给了一个字符串
+			:_size(strlen(str))   //这里先计算出字符串的大小
 		{
-			_capacity = _size;
-			_str = new char[_capacity + 1];
-			strcpy(_str, str);
+			_capacity = _size;   
+			_str = new char[_capacity + 1];//开空间的时候留一个位置给\0
+			strcpy(_str, str); //拷贝
 		}
 
-		~string()
+		~string() //析构
 		{
 			delete[] _str;
 			_str = nullptr;
@@ -363,28 +363,28 @@ namespace hdzc
 		}
 
 		// s1(s2)
-		string(const string& s)
+		string(const string& s) //拷贝构造
 		{
 			// +1是开空间时永远保持给\0多开一个
 			_str = new char[s._size + 1];
 			strcpy(_str, s._str);
-			_size = s._size;
+			_size = s._size; // size保持相等 
 			_capacity = s._size;
 
-			/*_str = new char[s._capacity + 1];
+			/*_str = new char[s._capacity + 1]; //两种方式都可以 
 			strcpy(_str, s._str);
 			_size = s._size;
 			_capacity = s._capacity;*/
 		}
 
 		// s1 = s2
-		string& operator=(const string& s)
+		string& operator= (const string& s) //operator 赋值
 		{
 			if (this != &s)
 			{
 				delete[] _str;
-				_str = new char[s._size + 1];
-				strcpy(_str, s._str);
+				_str = new char[s._size + 1]; //让s1的空间开成和s2一样大的 给/0多开一个空间
+				strcpy(_str, s._str);         //拷贝
 				_size = s._size;
 				_capacity = s._size;
 			}
@@ -392,7 +392,7 @@ namespace hdzc
 			return *this;
 		}
 
-		size_t size() const
+		size_t size() const   
 		{
 			return _size;
 		}
@@ -402,11 +402,11 @@ namespace hdzc
 			return _capacity;
 		}
 
-		char& operator[](size_t pos)
+		char& operator[](size_t pos)//这里是让string像数组一样去使用 
 		{
 			assert(pos < _size);
 
-			return _str[pos];
+			return _str[pos]; //返回的是当前的位置
 		}
 
 		const char& operator[](size_t pos) const
@@ -416,41 +416,42 @@ namespace hdzc
 			return _str[pos];
 		}
 
-		void reserve(size_t n)
+		void reserve(size_t n) //增容接口 方便后面的push和append使用
 		{
 			if (n > _capacity)
 			{
-				char* newstr = new char[n + 1];
-				strcpy(newstr, _str);
-				delete[] _str;
-				_str = newstr;
-
-				_capacity = n;
+				char* newstr = new char[n + 1]; //空间预留一个\0的位置 因为需要的有效字符的空间
+				strcpy(newstr, _str); //拷贝
+				delete[] _str;//旧空间释放
+				_str = newstr;//旧空间指向新空间
+				_capacity = n;//把capacity增加到n这么大
 			}
 		}
 
-		void resize(size_t n, char ch = '\0')
+		void resize(size_t n, char ch = '\0') 
 		{
-			if (n < _size) // 删除数据
+			if (n < _size) // n < size 的时候相当于删除数据
 			{
-				_size = n;
-				_str[_size] = '\0';
+				_size = n; //把size变成n
+				_str[_size] = '\0'; //最后一个位置给\0
 			}
-			else // 插入数据
+			else // n > size的时候插入数据
 			{
-				// 空间不够先增容
+				// 如果空间不够先增容
 				if (n > _capacity)
 					reserve(n);
 
-				for (size_t i = _size; i < n; ++i)
+				for (size_t i = _size; i < n; ++i)//空间足够的时候开始填数据
+				{
 					_str[i] = ch;
-				_size = n;
-				_str[_size] = '\0';
+				}
+					_size = n;
+					_str[_size] = '\0';		//size的最后一定要有\0			
 			}
 		}
 
-		// s1.push_back('x');
-		void push_back(char ch)
+		// s1.push_back('x'); // 这里只插入一个字符 增容增2倍一定是够的
+		void push_back(char ch)   //添加一个字符
 		{
 			// 增容
 			if (_size == _capacity)
@@ -465,17 +466,17 @@ namespace hdzc
 				}
 			}
 
-			_str[_size] = ch;
+			_str[_size] = ch;//_size表示最后一个字符的下一个位置 插入一个ch
 			++_size;
-			_str[_size] = '\0';
+			_str[_size] = '\0'; // ++_size 在后面要添加一个/0
 		}
 
 		// s1.append("xxxxxxxxxx");
-		void append(const char* str)
+		void append(const char* str) //添加多个字符
 		{
-			size_t len = strlen(str);
-			if (_size + len > _capacity)
-				reserve(_size + len);
+			size_t len = strlen(str);	//先去算一下插入的字符串长度是多少
+			if (_size + len > _capacity)//计算原有的长度 和 需要插入的长度 如果大于实际的空间容量 就进行增容
+				reserve(_size + len);	//增容
 
 			strcpy(_str + _size, str);
 			_size += len;
@@ -495,40 +496,40 @@ namespace hdzc
 			return *this;
 		}
 
-		string& insert(size_t pos, char ch)
+		string& insert(size_t pos, char ch) //在pos前一个位置进行插入一个字符 
 		{
-			assert(pos < _size);
-			if (_size == _capacity)
+			assert(pos < _size);    //要在size的范围之内进行插入
+			if (_size == _capacity) //考虑字符串的容量
 			{
-				reserve(_capacity * 2);
+				reserve(_capacity * 2);//满了进行开空间
 			}
 
-			size_t end = _size;
+			size_t end = _size;      //这里挪动数据要从最后往前开始挪动
 			while (end >= pos)
 			{
-				_str[end + 1] = _str[end];
-				--end;
+				_str[end + 1] = _str[end]; //前一个赋值给后一个
+				--end;             //不断向前移
 			}
 
-			_str[pos] = ch;
-			++_size;
+			_str[pos] = ch; // 开始放字符
+			++_size;         //最后因为增加了数据所以要++size
 
 			return *this;
 		}
 
-		string& insert(size_t pos, const char* str)
+		string& insert(size_t pos, const char* str)//在pos前一个位置进行插入字符串
 		{
-			assert(pos < _size);
-			size_t len = strlen(str);
-			if (_size + len > _capacity)
+			assert(pos < _size);					//在size范围内进行插入
+			size_t len = strlen(str);              //计算一下字符串str的长度
+			if (_size + len > _capacity)           //空间不足要增容
 			{
 				reserve(_size + len);
 			}
 
-			size_t end = _size;
+			size_t end = _size;                       //从最后开始向前挪动数据
 			while (end >= pos)
 			{
-				_str[end + len] = _str[end];
+				_str[end + len] = _str[end];          //一次性向后挪动len长度的数据
 				--end;
 			}
 
@@ -538,7 +539,7 @@ namespace hdzc
 			return *this;
 		}
 
-		void erase(size_t pos, size_t len = npos)
+		void erase(size_t pos, size_t len = npos)//
 		{
 			assert(pos < _size);
 
@@ -554,7 +555,7 @@ namespace hdzc
 			}
 		}
 
-		size_t find(char ch, size_t pos = 0)
+		size_t find(char ch, size_t pos = 0)//找单个字符
 		{
 			for (size_t i = pos; i < _size; ++i)
 			{
@@ -565,7 +566,7 @@ namespace hdzc
 			return npos;
 		}
 
-		size_t find(const char* str, size_t pos = 0)
+		size_t find(const char* str, size_t pos = 0)//找一个字符串
 		{
 			const char* p = strstr(_str + pos, str);
 			if (p == nullptr)
@@ -578,22 +579,22 @@ namespace hdzc
 		// 实现这两个，其他的比较复用实现
 		bool operator<(const string& s)
 		{
-			return strcmp(_str, s._str) < 0;
+			return strcmp(_str, s._str) < 0;//比较当前字符的ASCLL码
 		}
 
 		bool operator==(const string& s)
 		{
-			return strcmp(_str, s._str) == 0;
+			return strcmp(_str, s._str) == 0;//比较当前字符的ASCLL码
 		}
 
 		bool operator<=(const string& s)
 		{
-			return *this < s || *this == s;
+			return *this < s || *this == s;//比较当前字符的ASCLL码
 		}
 
 		bool operator>(const string& s)
 		{
-			return !(*this <= s);
+			return !(*this <= s);//比较当前字符的ASCLL码
 		}
 
 		bool operator>=(const string& s)
@@ -668,10 +669,10 @@ void test_string1()
 	}
 	cout << endl;
 
-	// 遍历+读写  范围for是有迭代器支持的，这里会被编译器转换成迭代器遍历
-	for (auto& ch : s1)
+	// 遍历+读写  范围for是有迭代器支持的，这里会被编译器转换成迭代器遍历 
+	for (auto& ch : s1) //范围for是需要支持迭代器的条件才可以使用
 	{
-		ch += 1;
+		ch += 1; // 可以把ch理解为*it1 这样就可以理解为
 		cout << ch << " ";
 	}
 	cout << endl;
